97be9e3d4586257942a6a87178806b41e9ef1c46
Erlantz <erlantzram@gmail.com>
2022-05-03 16:00:59 +0100

    Sinatra installed

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  "Hello!"
+end
+
+get '/secret' do
+  "This is secret!"
+end
+
+get '/other' do
+  "This is other page section!"
+end
