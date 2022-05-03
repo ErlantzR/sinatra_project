7aa373706f2963c48db5ad22909f6b1727e7058d
Farzan-I <farzan_15@hotmail.co.uk>
2022-05-03 16:24:22 +0100

    Step 8: Returning HTML done.

--- app.rb
+++ app.rb
@@ -12,3 +12,11 @@ end
 get '/other' do
   "This is other page section!"
 end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'> 
+    <img src='https://i.imgur.com/jFaSxym.png'>
+  </div>"
+end
+
+ 
